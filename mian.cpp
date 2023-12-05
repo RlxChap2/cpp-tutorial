6dc676a6dd96db11f1d10e465fa6732cf3ef941e
RlxChap2 <133292635+RlxChap2@users.noreply.github.com>
2023-12-05 17:30:14 +0300

    M

---
+++ mian.cpp
@@ -0,0 +1,59 @@
+// Input Output stream package
+#include <iostream>
+// Use STD For outputs and Inputs
+using namespace std;
+
+// Main Function Using (int)
+int main()
+{
+    // Print Hello World in console and end the line using (endl or \n)
+    cout << "Hello World!" << endl;
+    cout << "Hello World!"
+         << "\n";
+    // Print Hello World in console and make 3 spaces using (\t)
+    cout << "Hello\tWorld!"
+         << "\n";
+
+    /*
+    Variables :
+    {
+        int,
+         string
+          bool,
+           float,
+            double,
+    }
+    */
+
+    // int for (Numbers)
+    int x = 10;
+    cout << x << endl;
+
+    // String for (Names)
+    string name = "Mohammed";
+    cout << "My name is " << name << endl;
+
+    // bool for (true, false) - true = 1, false = 0
+    bool success = true;
+    cout << "Your answer is " << success << endl;
+    bool failed = false;
+    cout << "Your answer is " << failed << endl;
+
+    // float for numbers with (.) example (1.2222222) 7 numbers after (.)
+    float y = 1.25;
+    cout << y << endl;
+
+    // double for numbers with (.) example (1.2222222222222222) 14 numbers after (.)
+    double z = 2.25;
+    cout << z << endl;
+
+    // Cin
+    int i;
+    // make ask in the console
+    cin >> i;
+    // print the answer in the output
+    cout << i << endl;
+
+    // Stop the code
+    return 0;
+}
