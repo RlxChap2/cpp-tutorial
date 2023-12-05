42a87cd52b781742dfc40f255072e90bbff8c748
RlxChap2 <133292635+RlxChap2@users.noreply.github.com>
2023-12-05 19:23:00 +0300

    calc

--- src/SimpleCalc.cpp
+++ src/SimpleCalc.cpp
@@ -4,14 +4,46 @@ using namespace std;
 int main()
 {
     int x, y;
-    int sum;
+    char mark; // Change mark to a char type
 
-    cout << "Type a Number:";
+    cout << "Type a Number: ";
     cin >> x;
-    cout << "Type another Number:";
+    cout << "Type another Number: ";
     cin >> y;
-    sum = x + y;
-    cout << "Sum is: " << sum;
+    cout << "Type The Mark To Calculate (+, -, *, /): ";
+    cin >> mark; // Read in a character
+
+    int result; // Variable to store the result
+
+    // Use a switch statement to perform the operation based on the mark
+    switch (mark)
+    {
+    case '+':
+        result = x + y;
+        cout << "Sum is: " << result;
+        break;
+    case '-':
+        result = x - y;
+        cout << "Difference is: " << result;
+        break;
+    case '*':
+        result = x * y;
+        cout << "Product is: " << result;
+        break;
+    case '/':
+        if (y != 0) // Check for division by zero
+        {
+            result = x / y;
+            cout << "Division result is: " << result;
+        }
+        else
+        {
+            cout << "Error: Division by zero!";
+        }
+        break;
+    default:
+        cout << "Invalid operation!";
+    }
 
     return 0;
 }
